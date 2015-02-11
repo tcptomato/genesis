8cd96e32a4770548e252d65540615b86887d97c7
Thomas Böhm <boehm@fortiss.org>
2015-02-11 19:00:08 +0100

    Image processing and Latex

--- install.sh
+++ install.sh
@@ -5,3 +5,9 @@ sudo apt-get install fping nmap wireshark
 
 #development stuff
 sudo apt-get install build-essential subversion valgrind
+
+#iamge processing stuff
+sudo apt-get install imagemagick libopencv-dev gimp
+
+#latex
+sudo apt-get install texmaker
