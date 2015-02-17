a21cf3a99349b3c4e87ab4e8c388c63db03af30d
Thomas Böhm <boehm@fortiss.org>
2015-02-17 15:22:46 +0100

    Added clang

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 sudo apt-get install fping nmap wireshark
 
 #development stuff
-sudo apt-get install build-essential subversion valgrind
+sudo apt-get install build-essential subversion valgrind clang
 
 #iamge processing stuff
 sudo apt-get install imagemagick libopencv-dev gimp
@@ -12,4 +12,9 @@ sudo apt-get install imagemagick libopencv-dev gimp
 #latex
 sudo apt-get install texmaker
 
+#nano
 sudo apt-get install nano
+
+
+
+
